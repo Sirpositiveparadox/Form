06ef31018b0294a62fe7049986bc4e7d1fff8ee5
Dina Dolbina <dinadolbina@gmail.com>
2020-01-13 16:41:45 +0300

    Updates

---
+++ precache-manifest.d7e346660429601bc6c987ff4b2dbaeb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29d00c434d0ac8050cff244c5e9363a9",
+    "url": "sirpositiveparadpx.github.io/index.html"
+  },
+  {
+    "revision": "b6d384e2c731ddd6fa85",
+    "url": "sirpositiveparadpx.github.io/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "8f964128aa5f422e8415",
+    "url": "sirpositiveparadpx.github.io/static/css/main.96d74a0c.chunk.css"
+  },
+  {
+    "revision": "b6d384e2c731ddd6fa85",
+    "url": "sirpositiveparadpx.github.io/static/js/2.91fc5029.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "sirpositiveparadpx.github.io/static/js/2.91fc5029.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8f964128aa5f422e8415",
+    "url": "sirpositiveparadpx.github.io/static/js/main.05316fe2.chunk.js"
+  },
+  {
+    "revision": "f949e48603e0cf786595",
+    "url": "sirpositiveparadpx.github.io/static/js/runtime-main.5558cb2a.js"
+  }
+]);
