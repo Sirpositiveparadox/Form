6e3d9cff8df895315afec3e66ba9c2d03a2bab33
Dina Dolbina <dinadolbina@gmail.com>
2020-01-15 16:45:20 +0300

    origin

---
+++ build/precache-manifest.cae21abb4f3a9070374cdd888498febe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ee7ddf5c7972872ba49d4a14537589c",
+    "url": "/Form/index.html"
+  },
+  {
+    "revision": "adeaf62dfc9364f128dd",
+    "url": "/Form/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "43f76e52aacff517fdb5",
+    "url": "/Form/static/css/main.e4cdc4c7.chunk.css"
+  },
+  {
+    "revision": "adeaf62dfc9364f128dd",
+    "url": "/Form/static/js/2.bffc639c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Form/static/js/2.bffc639c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "43f76e52aacff517fdb5",
+    "url": "/Form/static/js/main.fda132e6.chunk.js"
+  },
+  {
+    "revision": "5f9d0ac30f02e57e5812",
+    "url": "/Form/static/js/runtime-main.38a6a16b.js"
+  }
+]);
