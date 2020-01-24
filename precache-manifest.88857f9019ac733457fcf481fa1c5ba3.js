5797416c8964b447e75d56ababd67c83e793e83b
Dina Dolbina <dinadolbina@gmail.com>
2020-01-24 13:46:33 +0300

    Updates

--- precache-manifest.88857f9019ac733457fcf481fa1c5ba3.js
+++ precache-manifest.88857f9019ac733457fcf481fa1c5ba3.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f9c7098adde3cf0a6e160b7b321ce63b",
+    "revision": "62c0098d86d4b21c9113d09791e13773",
     "url": "/Form/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Form/static/css/2.eb45ad44.chunk.css"
   },
   {
-    "revision": "fb5db3f042972a7ffa27",
-    "url": "/Form/static/css/main.185ad1a0.chunk.css"
+    "revision": "db9bde32c0927a74cadb",
+    "url": "/Form/static/css/main.e36b4481.chunk.css"
   },
   {
     "revision": "d84aa7f8f14aea326837",
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Form/static/js/2.6d8c695c.chunk.js.LICENSE"
   },
   {
-    "revision": "fb5db3f042972a7ffa27",
-    "url": "/Form/static/js/main.7e620194.chunk.js"
+    "revision": "db9bde32c0927a74cadb",
+    "url": "/Form/static/js/main.4fc7a368.chunk.js"
   },
   {
     "revision": "4ed293078123066cd6c0",
